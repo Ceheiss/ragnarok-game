66eef9180563171e915415dab3cea12c8475557d
Cristobal Heiss <barbacoabq@gmail.com>
2019-02-17 15:18:07 -0500

    created board and coordinates for each cell

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 console.log("Working JS")
+
+// Step One
+/*
+Create the grid
+Place barriers into the grid
+Place weapons into the grid
+Place players
+*/
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    $('.grid-container').append('<div class="grid-item" data-x='+i+' data-y='+j+'>Block at x='+i+' y='+j+'</div>')
+  }
+}
